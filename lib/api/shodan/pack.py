a0f3a8c54cc90da755406c7babed62e69cbdb673
unc1e <root@zuoxueba.org>
2022-03-20 17:47:57 +0800

    bugfix, uncomment shodan

--- lib/api/shodan/pack.py
+++ lib/api/shodan/pack.py
@@ -44,7 +44,7 @@ class ShodanBase:
         try:
             api = shodan.Shodan(self.api_key)
             result = api.search(query=self.query, offset=self.offset, limit=self.limit)
-        except APIError, e:
+        except APIError as e:
             sys.exit(logger.error(e))
 
         if 'matches' in result:
