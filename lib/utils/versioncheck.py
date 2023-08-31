6b05e9331bc1df341082ee2a2c17a4c6f1979fb2
unc1e <root@zuoxueba.org>
2023-08-31 23:29:53 +0800

    py2_To3: comment versioncheck.py

--- lib/utils/versioncheck.py
+++ lib/utils/versioncheck.py
@@ -12,7 +12,7 @@ import sys
 
 PYVERSION = sys.version.split()[0]
 
-if PYVERSION >= "3" or PYVERSION < "2.7":
-    exit("[CRITICAL] incompatible Python version detected ('%s'). "
-         "For successfully running this project, you'll have to use version 2.7"
-         "(visit 'http://www.python.org/download/')" % PYVERSION)
+# if PYVERSION >= "3" or PYVERSION < "2.7":
+#     exit("[CRITICAL] incompatible Python version detected ('%s'). "
+#          "For successfully running this project, you'll have to use version 2.7"
+#          "(visit 'http://www.python.org/download/')" % PYVERSION)
