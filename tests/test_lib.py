1ce15d8b429f56fc4284afad4f2555a130a53de0
unc1e <root@zuoxueba.org>
2023-09-06 22:23:32 +0800

    test: add fofa test

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,9 +1,18 @@
 import unittest
 
+from lib.api.fofa.pack import FofaSearch
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+
+
+class TestDork(unittest.TestCase):
+    def test_fofa(self):
+        results = FofaSearch("http", limit=100, offset=0)
+        self.assertIsNotNone(results)
+
 
 
 if __name__ == '__main__':
